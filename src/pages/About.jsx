005d4488d046437c11dbed0b98c5aa85ed5a3cfe
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-20 21:17:42 -0300

    feature: add pages Home, About and NotFound

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl font-light mb-5">GitHub Finder</h1>
+      <p>
+        This is a project to improve my skills with Javascript, React and CSS
+        (TailwindCSS and DaisyUI). Building a User Interface for GitHub Finder.
+      </p>
+    </div>
+  );
+}
+
+export default About;
