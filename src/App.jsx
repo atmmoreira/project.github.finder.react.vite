1adc11d4a78e292a4548f6dc28fe701650da30a1
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-23 09:13:24 -0300

    chore: indentation

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import { GithubProvider } from "./context/github/GithubContext";
 // Context
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
